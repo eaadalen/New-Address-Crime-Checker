93f1371e7b96f3771110961d558e803c593bd488
eaadalen <eaadalen112@gmail.com>
2024-12-16 18:41:07 -0600

    Add netlify URl

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const port = process.env.PORT || 4242
 const crime_data = require('../client/assets/crime_data/json/sorted_crime_data.json');
 
-let allowedOrigins = ['http://localhost:1234']
+let allowedOrigins = ['http://localhost:1234','https://safemove.netlify.app']
 
 app.use(cors({
   origin: (origin, callback) => {
