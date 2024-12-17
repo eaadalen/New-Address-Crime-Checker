a99d1182bf625348756a524b008155c2a63f5835
eaadalen <eaadalen112@gmail.com>
2024-12-17 13:46:18 -0600

    TS

--- client/assets/crime_data/scripts/get_fresh_data.js
+++ client/assets/crime_data/scripts/get_fresh_data.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 async function downloadCrimeData() {
     // Launch a new browser instance
-    const browser = await puppeteer.launch({ pexecutablePath: puppeteer.executablePath() });
+    const browser = await puppeteer.launch({ executablePath: puppeteer.executablePath() });
     const page = await browser.newPage();
 
     // Set the download path to the same directory as the script
