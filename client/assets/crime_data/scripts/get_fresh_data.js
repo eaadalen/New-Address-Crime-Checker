7625350f5524fca611ce967d9a630eed10eb72ff
eaadalen <eaadalen112@gmail.com>
2024-12-21 22:22:11 -0600

    TS

---
+++ client/assets/crime_data/scripts/get_fresh_data.js
@@ -0,0 +1,68 @@
+const puppeteer = require('puppeteer-core');
+const os = require('os');
+
+async function downloadCrimeData() {
+    // Launch a new browser instance
+    const browser = await puppeteer.launch({
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ],
+        executablePath: process.env.GOOGLE_CHROME_BIN || null,
+        headless: true
+    });
+    const page = await browser.newPage();
+
+    // Set the download path to the same directory as the script
+    let downloadPath = __dirname.replace("\\scripts", "") + "\\csv"; // Escape backslashes
+    
+    await page._client().send('Page.setDownloadBehavior', {
+        behavior: 'allow',
+        downloadPath: downloadPath
+    });
+
+    // Go to the specified URL
+    await page.goto('https://opendata.minneapolismn.gov/datasets/cityoflakes::crime-data/explore?location=11.676552%2C-46.664577%2C2.34', { waitUntil: 'networkidle2' });
+
+    // Wait for the SVG element to be available in the DOM by its ID
+    await page.waitForSelector('svg#ember114', { visible: true });
+
+    // Locate and click the SVG element
+    const svgElement = await page.$('svg#ember114');
+    if (svgElement) {
+        await svgElement.click({ force: true });
+    } else {
+        console.log("SVG not found.");
+        await browser.close();
+        return;
+    }
+
+    // Wait for the arcgis-hub-download-list element
+    const downloadListHost = await page.waitForSelector('arcgis-hub-download-list', { visible: true });
+    
+    // Access the shadow root of the arcgis-hub-download-list
+    const downloadListShadowRoot = await downloadListHost.evaluateHandle(host => host.shadowRoot);
+
+    // Wait for the arcgis-hub-download-list-item elements
+    const downloadItemHost = await downloadListShadowRoot.evaluateHandle(root => root.querySelector('arcgis-hub-download-list-item'));
+    const downloadItemShadowRoot = await downloadItemHost.evaluateHandle(host => host.shadowRoot);
+
+    // Now, look for the Calcite button inside the shadow root
+    const csvDownloadButton = await downloadItemShadowRoot.$('calcite-button');
+
+    if (csvDownloadButton) {
+        // Click the button
+        await csvDownloadButton.click({ force: true });
+        console.log("CSV Download Initiated");
+    } else {
+        console.log("CSV download button not found.");
+    }
+
+    // Keep the browser open for a few seconds to allow time for the download
+    await new Promise(resolve => setTimeout(resolve, 25000));
+
+    // Close the browser
+    await browser.close();
+}
+
+module.exports = downloadCrimeData
---
+++ client/assets/crime_data/scripts/remove_unnecessary_columns.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const Papa = require('papaparse');
+
+// Function to remove columns from CSV and remove rows with Latitude = 0
+function removeColumnsFromCSV() {
+    return new Promise((resolve) => {
+        console.log('CSV Download Completed')
+        // Read the CSV file
+        let downloadPath = __dirname.replace("\\scripts", "") + "\\csv"; // Escape backslashes
+        const csvFile = fs.readFileSync(downloadPath + "\\Crime_Data.csv", 'utf8');
+        const columnsToRemove = ['X', 'Y', 'Type', 'Case_Number', 'Case_NumberAlt', 'Reported_Date', 'NIBRS_Crime_Against', 'NIBRS_Group', 'NIBRS_Code', 'Offense_Category', 'Problem_Initial', 'Problem_Final', 'Address', 'Precinct', 'Neighborhood', 'Ward', 'wgsXAnon', 'wgsYAnon', 'Crime_Count', 'OBJECTID'];   // Replace with the names of columns you want to remove
+
+        // Parse the CSV data
+        const parsedData = Papa.parse(csvFile, { header: true });
+
+        // Filter out rows with Latitude = 0
+        const filteredRows = parsedData.data.filter(row => Number(row.Latitude) !== 0);
+
+        // Filter out the columns to remove
+        const filteredData = filteredRows.map(row => {
+            const filteredRow = {};
+            Object.keys(row).forEach(key => {
+                if (!columnsToRemove.includes(key)) {
+                    filteredRow[key] = row[key];
+                }
+            });
+            return filteredRow;
+        });
+
+        // Convert the filtered data back to CSV format
+        const newCsv = Papa.unparse(filteredData);
+
+        // Write the new CSV to the output file
+        fs.writeFileSync(downloadPath + "\\Optimized_Crime_Data.csv", newCsv);
+        console.log(`Unnecessary columns removed and rows with Latitude = 0 filtered out. New file saved as: ${'../client/assets/crime_data/csv/Optimized_Crime_Data.csv'}`);
+        resolve()
+    });
+}
+
+module.exports = removeColumnsFromCSV
