c3cacfae6ab84fe6f088ccb960a217320d91463e
eaadalen <eaadalen112@gmail.com>
2024-12-21 22:19:09 -0600

    TS

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const Models = require('./models.js')
 const cors = require('cors')
 const port = process.env.PORT || 4242
-//const crime_data = require('../client/assets/crime_data/json/sorted_crime_data.json');
+const crime_data = require('../client/assets/crime_data/json/sorted_crime_data.json');
 
 app.use(cors({
   //origin: 'http://localhost:1234'
