5fdb68d11720cf1ad5060bf58ebca9c547a20512
eaadalen <eaadalen112@gmail.com>
2024-12-21 23:04:07 -0600

    TS

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,4 @@
-require('dotenv').config()
+//require('dotenv').config()
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 import "./index.css";
